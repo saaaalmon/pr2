5fc079bd603af3da52b3c9f350a5b5f26375626b
saaaalmon <salmonsaidolimov0@gmail.com>
2024-09-07 10:59:16 +0300

    Практика 2

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+'use strict';
+// Задание 1
+//  let name = 'Jon';
+//  let admin = (`${name}`);
+
+//  alert (`Name ${admin}`);   
+
+// Задание 2
+// let name = prompt('Как наз-ся ваш город');
+// let year = prompt('В каком году образовался ваш город');
+// let population = prompt('Сколько чел живут в вашем городе');
+
+// let num = 2024 - (`${year}`);
+
+// alert(`Горолу ${name} исполнилось ${num} лет с момента его образования. Население - ${population} человек`);
+
+// Задание 3
+// let p = 3.14;
+// let r = prompt('Какой радиус у круга');
+// let num = p*(r*r);
+// alert (`Радиус ${r}. Площадь ${num}`);  
+
+// Задание 4
+// let ac = prompt('1 число');
+// let bc = prompt('2 число');
+
+// let a = Number(ac);
+// let b = Number(bc);
+
+// let sum = a+b;
+// let min = a-b;
+// let mn = a*b; 
+// let dl = a/b;
+
+// alert(`Сумма ${sum}, разность ${min}, умножить ${mn}, делить ${dl}`);  
